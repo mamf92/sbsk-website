cffcf3831f864d1a2d9fa215deb0ccdd96c4871a
Martin Fischer <mamfischer92@gmail.com>
2026-05-20 09:27:45 +0200

    chore: add update to schema for posts

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -1,4 +1,5 @@
 import { defineField, defineType } from 'sanity';
+import ExternalLink from '../../assets/icons/symbols/external-link.svg?react';
 
 export const postType = defineType({
   name: 'post',
@@ -23,14 +24,56 @@ export const postType = defineType({
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: 'images',
+      title: 'Innhold',
+      name: 'content',
       type: 'array',
-      of: [{ type: 'image' }],
-    }),
-    defineField({
-      name: 'body',
-      type: 'array',
-      of: [{ type: 'block' }],
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'Heading 1', value: 'h2' },
+            { title: 'Heading 2', value: 'h3' },
+            { title: 'Quote', value: 'blockquote' },
+          ],
+          marks: {
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Underline', value: 'underline' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+            annotations: [
+              {
+                name: 'link',
+                type: 'object',
+                title: 'link',
+                fields: [
+                  {
+                    name: 'url',
+                    type: 'url',
+                  },
+                ],
+              },
+              {
+                name: 'internalLink',
+                type: 'object',
+                title: 'Intedrnal link',
+                icon: ExternalLink,
+                fields: [
+                  {
+                    name: 'reference',
+                    type: 'reference',
+                    to: [{ type: 'event' }],
+                  },
+                ],
+              },
+            ],
+          },
+        },
+        {
+          type: 'image',
+        },
+      ],
     }),
   ],
 });
